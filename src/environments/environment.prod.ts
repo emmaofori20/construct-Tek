7dd25c836ebe3e12293193b13f8082217b859871
emmaofori20 <emmanuelofori2638@gmail.com>
2021-07-26 20:25:14 +0000

    version 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+  apiKey: "AIzaSyBCufcIu7ybop41gYh0l2F-KOsOo3ngtKE",
+  authDomain: "construct-tek.firebaseapp.com",
+  projectId: "construct-tek",
+  storageBucket: "construct-tek.appspot.com",
+  messagingSenderId: "694084065236",
+  appId: "1:694084065236:web:fcf2cebb08f06f68bde841",
+  measurementId: "G-3VZBM1VMYC"
+}
 };
